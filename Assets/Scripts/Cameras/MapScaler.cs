bf562f91d6abf732aca7e6a4983eb09cecd94605
Ricardo Bustamante <ricardo@busta.dev>
2021-07-09 00:17:23 +0200

    Add game map. Add post processing. Update materials to URP

--- Assets/Scripts/Cameras/MapScaler.cs
+++ Assets/Scripts/Cameras/MapScaler.cs
@@ -7,7 +7,7 @@ namespace Cameras
     public class MapScaler : MonoBehaviour
     {
         [SerializeField] private MiniMap miniMap;
-        [SerializeField] private Transform floorTransform;
+
         [SerializeField] private Camera miniMapCamera;
 
         [SerializeField] private float scale;
@@ -15,9 +15,7 @@ namespace Cameras
 
         private void Start()
         {
-            var scale2 = scale * 2;
             miniMap.SetScale(scale);
-            floorTransform.localScale = new Vector3(scale2, 1, scale2);
             miniMapCamera.orthographicSize = scale;
 
             LocalRtsPlayer.GetLocalPlayerAsync(player =>
